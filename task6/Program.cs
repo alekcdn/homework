051ed080a3d9df5e5daa80db86b9d91ad9978d25
alekcdn <alex3l@mail.ru>
2022-07-11 13:30:47 +0300

    add task6

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int a;
+Console.Write("Введите число : ");
+a = Convert.ToInt32(Console.ReadLine());
+ 
+    if (a % 2 == 0)
+    {
+    Console.Write("Это чётное число");
+    }
+        else
+        {
+        Console.Write("Это нечетное число");
+        }
