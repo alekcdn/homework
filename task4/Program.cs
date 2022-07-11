5de5d13ca5a5779f9c851857a9656e9c4d3dc4e6
alekcdn <alex3l@mail.ru>
2022-07-11 13:13:42 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿int a;
+int b;
+int c;
+int max;
+
+Console.WriteLine("Введите три целых числа : ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+Console.WriteLine("max= {0}", max);
