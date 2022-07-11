8e5175cb799ef64ef33004ceab5a2e9d5d422d16
alekcdn <alex3l@mail.ru>
2022-07-11 15:02:25 +0300

    add task8

---
+++ task8/Program.cs
@@ -0,0 +1,16 @@
+﻿int N;
+Console.WriteLine("Введите число: ");
+N = Convert.ToInt32(Console.ReadLine());
+int i = 1;            
+while (true)
+{
+	if (i % 2 == 0)
+	{
+		Console.Write(i + " ");
+	}
+	if (i >= N)
+	{
+		break;
+	}
+	i++;
+}
