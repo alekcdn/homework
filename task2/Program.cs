eadbbf2b4b172c9ee8f3dcbeda0ffcbb39970bd3
alekcdn <alex3l@mail.ru>
2022-07-11 12:44:34 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int a;
+int b;
+ 
+Console.WriteLine("Введите первое число: ");
+ a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+ b = Convert.ToInt32(Console.ReadLine());
+ 
+    if (a > b)
+        {
+        Console.WriteLine("{0} это наибольшее число", a);
+        }
+ 
+        else
+ 
+            {
+            Console.WriteLine("{0} это наибольшее число ", b);
+            }
+ 
+
