2c8d972fd06a53cd1a036768aac29ad077d30789
alekcdn <alex3l@mail.ru>
2022-08-08 12:13:03 +0300

    add task 34

---
+++ task34/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет 
+// количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void FindEvenElem(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count++;
+    }
+    Console.WriteLine($"Количество чётных элементов массива = {count}");
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i] + "]");
+    }
+}
+int[] array = CreateArrayRndInt(size, 100, 999);
+Console.WriteLine("Создан массив:");
+PrintArray(array);
+Console.WriteLine();
+FindEvenElem(array);
+
+
